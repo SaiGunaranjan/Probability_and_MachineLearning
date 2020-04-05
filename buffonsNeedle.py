123ca1c20cb1bd13a9dc33cbe0689e58de4fe228
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2020-04-05 19:02:23 +0530

    Fix bug in generating needle centres
    In this commit, I have made the following changes which has now resulted
    in better estimate of pi and hence the probability of needle lying on a
    line.
    1. Removed the extra L/2 from the start and stop index for the needle
    centres. It was probably the bug which was causing the error/bias in the
    previous commit. Hence it has been removed.
    2. Vectorized the checking criterion for needle to fall on line.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- buffonsNeedle.py
+++ buffonsNeedle.py
@@ -10,20 +10,18 @@ import matplotlib.pyplot as plt
 
 plt.close('all')
 
-
-maxnumNeedles = np.int32(1e3)
+maxnumNeedles = np.int32(1e6)
 needleLength = 5
 lineSpacing = 2*needleLength
 lineInd = 100
 verticalSpacedLines = np.arange(-lineInd,lineInd,lineSpacing);
-needleCentreInd_start = verticalSpacedLines[0] - needleLength/2;
-needleCentreInd_end = verticalSpacedLines[-1] + needleLength/2;
+needleCentreInd_start = verticalSpacedLines[0];
+needleCentreInd_end = verticalSpacedLines[-1];
 
-step=10
+step = np.int32(1e4)
 numNeedles = np.arange(10,maxnumNeedles,step)
 
-
-numMonteCarloRuns = 100
+numMonteCarloRuns = 10
 piApproxVec = np.zeros((len(numNeedles),),dtype=np.float32)
 count = 0
 for needNum in numNeedles:
@@ -31,17 +29,15 @@ for needNum in numNeedles:
     for run in np.arange(numMonteCarloRuns):
         needleTheta = np.random.uniform(low=0,high=np.pi,size=needNum);
         needleCentre = np.random.uniform(low=needleCentreInd_start,high=needleCentreInd_end,size=needNum);
-        numTouchingNeedles=0
-        for ele in np.arange(needNum):
-            temp1 = verticalSpacedLines >= (needleCentre[ele] - (needleLength/2)*np.sin(needleTheta[ele]));
-            temp2 = verticalSpacedLines <= (needleCentre[ele] + (needleLength/2)*np.sin(needleTheta[ele]));
-            temp = temp1*temp2;
-            if np.sum(temp)==1: 
-                numTouchingNeedles+=1
+    
+        temp1 = verticalSpacedLines[None,:] >= (needleCentre - (needleLength/2)*np.sin(needleTheta))[:,None];
+        temp2 = verticalSpacedLines[None,:] <= (needleCentre + (needleLength/2)*np.sin(needleTheta))[:,None];
+        temp = temp1 * temp2;
+        numTouchingNeedles = np.sum(temp)
         piApprox += (2*needleLength/lineSpacing)*(needNum/numTouchingNeedles)
+        
     piApproxVec[count] = piApprox/numMonteCarloRuns
     count+=1
-
 
 probNeedleCross = (2*needleLength/lineSpacing)/piApproxVec
 
@@ -53,12 +49,15 @@ probNeedleCross = (2*needleLength/lineSpacing)/piApproxVec
 
 plt.figure(2,figsize=(20,10));
 plt.subplot(1,2,1)
+plt.title('Estimated pi value');
 plt.plot(numNeedles,piApproxVec,'-o');
+plt.axhline(np.pi,color='k',linewidth=2)
 plt.xlabel('Total number of needles')
-plt.ylabel('pi approximation');
+
 plt.grid(True)
 plt.subplot(1,2,2)
+plt.title('Probability of needle touching line')
 plt.plot(numNeedles,probNeedleCross,'-o');
+plt.axhline(1/np.pi,color='k')
 plt.xlabel('Total number of needles');
-plt.ylabel('Probability of needle touching line')
 plt.grid(True)
