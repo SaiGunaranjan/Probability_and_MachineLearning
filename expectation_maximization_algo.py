be7088ff3fa72a6193d4223d7fb7fc8b2db7beba
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2023-06-08 18:53:20 +0530

    Modified the code with better understanding of the EM algorithm
    In this commit, I have made minor modifications to the EM algorithm code
    with a better understanding of the algorithm based on the video lectures
    of Arun Rajkumar. No mah=jor changes. Now, Im computing the priors also
    at each iteration step. But somehow, the priors estimated are still not
    converging. Also, if the variance of the GT is changed, then also the
    algo is not converging. I need to debug this.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- expectation_maximization_algo.py
+++ expectation_maximization_algo.py
@@ -5,6 +5,15 @@ Created on Sat Jun  3 23:45:29 2023
 @author: Sai Gunaranjan
 """
 
+
+""" This code contains the 1d implementation of the popular Expectation Maximization(EM) algorithm.
+I have implemented this algorithm based on the videos of Arun Rajkumar of IIT Madras. The algorithm is
+beautifully explained in the video lectures. I have also referred the EM video lecture by Alexander Ihler for the code implementation part.
+The derivation of this algo is available in Arun's videos and also in the CS229 course notes of Andrew NG.
+In the course notes, he explains how the convex combination used in the Jensen's inequality can be interpreted as an Expectation.
+For a concave function f(x), f(E[x]) >= E[f(x)]. This is a very crucial interpretation and this is how the Log likelihood of a mixture of
+gaussians is converted to a simple form which can be analytically computed"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -35,25 +44,41 @@ plt.grid(True)
 # plt.close('all')
 
 dataPointsArray = np.sort(dataPointsArray) # Sorting the datapoints for subsequenct plotting convenience
+numDataPoints = len(dataPointsArray)
 """EM algorithm """
 
-""" Step 1: Initializtion of parameters"""
+""" Step 1: Initialization of parameters"""
 meanArrayEM = np.array([-20,20]) # np.array([-5,10])
 varianceArrayEM = np.array([2,2])
+priorProbArrayEM = np.array([0.5,0.5])
+
+print('Iteration: {}'.format(0))
+print('Mean', meanArrayEM)
+print('Variance', varianceArrayEM)
+print('\n')
+
+numIterations = 15
+logLikelihoodArray = np.zeros((numIterations))
 plt.figure(3,figsize=(20,10))
-for iterCount in range(15):
-    """ Step 2: Compute the posteriors of the latent random variables (P(z/x))"""
+for iterCount in range(numIterations):
+    """ Step 2: E step. Compute the posteriors(labda_ij) of the latent random variables (P(z/x))"""
     """ For this, we need to compute P(x/z)"""
 
-    Pxcondz =  1/(np.sqrt(2 * np.pi * varianceArrayEM[None,:]**2)) * np.exp( - (dataPointsArray[:,None] - meanArrayEM[None,:])**2 / (2 * varianceArrayEM[None,:]**2))
-    Pzcondx = Pxcondz/np.sum(Pxcondz,axis=1)[:,None]
+    Pxcondz =   1/(np.sqrt(2 * np.pi * varianceArrayEM[None,:]**2)) * np.exp( - (dataPointsArray[:,None] - meanArrayEM[None,:])**2 / (2 * varianceArrayEM[None,:]**2))
+    Px = np.sum(Pxcondz,axis=1)
+    Pzcondx =  priorProbArrayEM[None,:] * (Pxcondz / Px[:,None]) # lamda_ij (index i for the data point and index j for the cluster)
+    logLikelihood = np.sum(np.log10(Px))
+    logLikelihoodArray[iterCount] = logLikelihood
 
-    """ Step 3: Re-estimate the parameters using the posteriors"""
+    """ Step 3: M step. Re-estimate the parameters using the posteriors"""
     meanArrayEM = np.sum(dataPointsArray[:,None] * Pzcondx, axis=0)/ np.sum(Pzcondx,axis=0)
     varianceArrayEM = np.sum(((dataPointsArray[:,None] - meanArrayEM[None,:])**2) * Pzcondx,axis=0)/ np.sum(Pzcondx,axis=0)
+    priorProbArrayEM = np.sum(Pzcondx,axis=0)/numDataPoints
+
     print('Iteration: {}'.format(iterCount+1))
     print('Mean', meanArrayEM)
     print('Variance', varianceArrayEM)
+    print('Priors', priorProbArrayEM)
     print('\n')
     plt.clf()
     plt.subplot(1,2,1)
