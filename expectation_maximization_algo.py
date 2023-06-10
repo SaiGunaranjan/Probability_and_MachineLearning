c284f318e5a8515342746a9133527009f3a32852
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2023-06-10 17:30:39 +0530

    Generate data points from cluster based on corresponding priors
    In this commit, I am generating data points from each cluster based on
    the priors for that cluster. Right now, I have coded for a 2 gaussian
    mixture model. So for this, if the priors for the 2 gaussian mixtures
    are 0.8, 0.2, then, we generate 80 percent of the data points from
    cluster 1 and 20 percent of the points from cluster 2. Also, while
    plotting the posteriors of the latent variables, Im plotting on the log
    scale. But one thing remains to be debugged and that is the convergence
    of the prior probabilities. Somehow, the estimated priors are not
    matching the programmed priors.
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- expectation_maximization_algo.py
+++ expectation_maximization_algo.py
@@ -22,16 +22,19 @@ plt.close('all')
 """ Data generation using Gaussian Mixture Model"""
 numClusters = 2
 priors = 0.5*np.ones((numClusters))
-meanArray = np.array([-10,15])
+meanArray = np.array([7,15])
 varianceArray = np.array([1,1])
-numDataPointsEachCluster = np.array([10000,10000])
-
+priorsArray = np.array([0.8,0.2])
+numDataPoints = 20000
+numDataPointsEachCluster = (priorsArray * numDataPoints).astype('int32') #np.array([10000,10000])
+numDataPoints = np.sum(numDataPointsEachCluster)
 
 dataPointsArray = np.empty([0])
 plt.figure(1,figsize=(20,10))
 for ele in range(numClusters):
     dataPoints = np.random.normal(meanArray[ele], varianceArray[ele], numDataPointsEachCluster[ele])
     count,bins,_ = plt.hist(dataPoints,bins=100,density=True)
+    # plt.plot(bins, priorsArray[ele] * 1/(np.sqrt(2 * np.pi * varianceArray[ele]**2)) * np.exp( - (bins - meanArray[ele])**2 / (2 * varianceArray[ele]**2) ),linewidth=2, color='black')
     plt.plot(bins, 1/(np.sqrt(2 * np.pi * varianceArray[ele]**2)) * np.exp( - (bins - meanArray[ele])**2 / (2 * varianceArray[ele]**2) ),linewidth=2, color='black')
     plt.grid(True)
     dataPointsArray = np.append(dataPointsArray,dataPoints)
@@ -41,14 +44,14 @@ plt.figure(2,figsize=(20,10))
 plt.title('Data distribution')
 count,bins,_ = plt.hist(dataPointsArray,bins=200,density=True)
 plt.grid(True)
-# plt.close('all')
+
 
 dataPointsArray = np.sort(dataPointsArray) # Sorting the datapoints for subsequenct plotting convenience
-numDataPoints = len(dataPointsArray)
+
 """EM algorithm """
 
 """ Step 1: Initialization of parameters"""
-meanArrayEM = np.array([-20,20]) # np.array([-5,10])
+meanArrayEM = np.array([4,17]) # np.array([-5,10])
 varianceArrayEM = np.array([2,2])
 priorProbArrayEM = np.array([0.5,0.5])
 
@@ -80,20 +83,26 @@ for iterCount in range(numIterations):
     print('Variance', varianceArrayEM)
     print('Priors', priorProbArrayEM)
     print('\n')
+
     plt.clf()
     plt.subplot(1,2,1)
     plt.title('P(x/z)')
     # plt.plot(bins, 1/(np.sqrt(2 * np.pi * varianceArrayEM[None,:]**2)) * np.exp( - (bins[:,None] - meanArrayEM[None,:])**2 / (2 * varianceArrayEM[None,:]**2) ),linewidth=2, color='black')
-    plt.plot(dataPointsArray,Pxcondz/np.sum(Pxcondz))
-    # plt.ylim([-0.25,1])
+    plt.plot(dataPointsArray,Pxcondz/np.sum(Pxcondz,axis=0)[None,:])
+    plt.ylim([0,2e-4])
     plt.grid(True)
 
     plt.subplot(1,2,2)
-    plt.title('P(z/x)')
-    plt.plot(dataPointsArray,Pzcondx)
-    plt.ylim([-0.25,1.5])
+    plt.title('P(z/x) in log scale')
+    plt.plot(dataPointsArray,np.log10(Pzcondx))
+    plt.ylim([-50,0])
     plt.grid(True)
 
     plt.pause(1)
 
 
+plt.figure(4,figsize=(20,10))
+plt.title('Log likelihood')
+plt.plot(logLikelihoodArray,'-o')
+plt.xlabel('Iteration number')
+plt.grid(True)
