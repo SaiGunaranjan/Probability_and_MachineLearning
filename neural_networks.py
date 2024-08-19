cc375bf78739a8dd7ee032ee9d1c74259d1bc641
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2024-08-19 22:36:53 +0530

    Added some dummy training data to check if the neural network code runs without crashing
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- neural_networks.py
+++ neural_networks.py
@@ -150,8 +150,10 @@ class MLFFNeuralNetwork():
 
 
 
-
-
+mlffnn = MLFFNeuralNetwork()
+trainData = np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
+trainDataLabels = np.array([[1,0],[0,1],[0,0]])
+mlffnn.train_nn(trainData,trainDataLabels)
 
 
 
