38c37b1d9c95abab8e82c0afc7b974fc08ce1f03
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2024-08-19 22:59:12 +0530

    Create a user defined NN architecture
    Following are the features
    1. Can program the number of hidden layers, number of nodes per hidden layer, activation function for each layer. All passed as a list. List of #nodes, activation function tuples. Length of the list indicates the total number of layers of the neural network. The entries of the first and last elements of the list denote the input layer and output layer respectively.
    
    2. Defined a function named 'set_model_params' where we can decide the following:
        1. Type of gradient descent for the training process like batch, online/stochastic, mini batch, etc.
        2. Cost function to optimize i.e categorical cross entropy, squared error loss, etc. Typically, we use categorical cross entropy for classification tasks and squared error loss furnction for regression tasks
        3. Number of epochs of the training
    
    3. Broke down the backpropagation function into sub functions namely:
        1. Forward pass
        2. Cost function evaluation post forward pass
        3. Backward pass
        4. Update weights
    
    4. Both forward pass and backward pass can now can cater to any number of user defined layers in the network through a for loop
    
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- neural_networks.py
+++ neural_networks.py
@@ -17,7 +17,27 @@ I have also derived based on the above link. The derivation is availabe in my On
     Machine Learning notes tab, Backpropagation algorithm page
 
 
-Multi class classification using softmax and cross entropy.
+Multi class classification using softmax and categorical cross entropy.
+
+Updates:
+
+05/08/2024
+The script can now create a user defined NN architecture
+Following are the features
+1. Can program the number of hidden layers, number of nodes per hidden layer, activation function for each layer. All passed as a list. List of #nodes, activation function tuples. Length of the list indicates the total number of layers of the neural network. The entries of the first and last elements of the list denote the input layer and output layer respectively.
+
+2. Defined a function named 'set_model_params' where we can decide the following:
+    1. Type of gradient descent for the training process like batch, online/stochastic, mini batch, etc.
+    2. Cost function to optimize i.e categorical cross entropy, squared error loss, etc. Typically, we use categorical cross entropy for classification tasks and squared error loss furnction for regression tasks
+    3. Number of epochs of the training
+
+3. Broke down the backpropagation function into sub functions namely:
+    1. Forward pass
+    2. Cost function evaluation post forward pass
+    3. Backward pass
+    4. Update weights
+
+4. Both forward pass and backward pass can now can cater to any number of user defined layers in the network through a for loop
 
 
 
@@ -36,6 +56,9 @@ NN for classification problem
 5. Batch vs online vs mini batch mode of gradient descent
 6. Cross validation with validation dataset (k fold cross validation)
 7. How are Accuracy and loss curves computed on the validation dataset
+8. Make provision for batch mode and mini batch mode of training as well
+9. Clean up multiple calls of the is else for the activation and derivative of activation function. [Done]
+10. Compare my implementation of the forward pass with tensorflow/pytorch implementation
 """
 
 import numpy as np
@@ -43,23 +66,24 @@ np.random.seed(0)
 
 class MLFFNeuralNetwork():
 
-    def __init__(self):
+    def __init__(self,networkArchitecture):
 
-        # layerType = 'input'
-        # activationFunction = 'identity'
-        numFeatures = 5 #example
-        self.numNodesLayer1 = numFeatures #numNodesInputLayer
+        self.networkArchitecture = networkArchitecture
+        """ weights initialization between each successive layers"""
+        self.numLayers = len(self.networkArchitecture)
+        self.weightMatrixList = []
+        for ele in range(self.numLayers-1):
+            """Weight matrix from layer l to layer l+1 """
+            numNodesLayerL = self.networkArchitecture[ele][0]
+            numNodesLayerLplus1 = self.networkArchitecture[ele+1][0]
+            """ Initialize the weights matrix to 0s. Other initializations like picking from a normal distribution are also possible"""
+            weightMatrix = np.zeros((numNodesLayerLplus1,numNodesLayerL+1),dtype=np.float32) # +1 is for the bias term
+            self.weightMatrixList.append(weightMatrix)
 
-        """ Initialize the weights matrix to 0s. Other initializations like picking from a normal distribution are also possible"""
-        self.numNodesLayer2 = 128
-        self.weightMatrixLayer1to2 = np.zeros((self.numNodesLayer2,self.numNodesLayer1+1),dtype=np.float32) # +1 is for the bias term
-
-        self.numNodesLayer3 = 128
-        self.weightMatrixLayer2to3 = np.zeros((self.numNodesLayer3,self.numNodesLayer2+1),dtype=np.float32) #+1 is for the bias term
-
-        self.numNodesLayer4 = 3 # numNodesOutputLayer #output is a 1 hot vector of 3 classes
-        self.weightMatrixLayer3to4 = np.zeros((self.numNodesLayer4,self.numNodesLayer3+1),dtype=np.float32) #+1 is for the bias term
-
+    def set_model_params(self,mode = 'online',costfn = 'categorical_cross_entropy',epochs = 10):
+        self.mode = mode
+        self.costfn = costfn
+        self.epochs = epochs
 
 
     def sigmoid(self,z):
@@ -71,13 +95,15 @@ class MLFFNeuralNetwork():
         ePowmz = np.exp(-z)
         return (ePowpz - ePowmz) / (ePowpz + ePowmz)
 
+    def ReLU(self,z):
+        return np.maximum(0,z)
+
     def softmax(self,z):
         """ z has to be a vector"""
         ePowz = np.exp(z)
         return ePowz/np.sum(ePowz)
 
-    def ReLU(self,z):
-        return np.maximum(0,z)
+
 
     def sigmoid_derivative(self,z):
         return self.sigmoid(z) * (1-self.sigmoid(z))
@@ -88,69 +114,150 @@ class MLFFNeuralNetwork():
     def ReLU_derivative(self,z):
         return 1*(z>0) # Returns 0 for negative values, 1 for posotive values
 
-    def forwardpass(self):
-        pass
+    def activation_function(self, itaLayerL, activationFn):
+        if (activationFn == 'sigmoid'):
+            layerLOutput = self.sigmoid(itaLayerL)
+        elif (activationFn == 'tanh'):
+            layerLOutput = self.tanh(itaLayerL)
+        elif (activationFn == 'ReLU'):
+            layerLOutput = self.ReLU(itaLayerL)
+        elif (activationFn == 'softmax'):
+            layerLOutput = self.softmax(itaLayerL)
 
-    def backwardpass(self):
-        pass
+        return layerLOutput
+
+    def derivative_activation_function(self, itaLayerL, activationFn):
+        if (activationFn == 'sigmoid'):
+            activationFnDerivative = self.sigmoid_derivative(itaLayerL)
+        elif (activationFn == 'tanh'):
+            activationFnDerivative = self.tanh_derivative(itaLayerL)
+        elif (activationFn == 'ReLU'):
+            activationFnDerivative = self.ReLU_derivative(itaLayerL)
+
+        return activationFnDerivative
+
+
+    def compute_loss_function(self,trainDataLabel):
+
+        if (self.costfn == 'categorical_cross_entropy'):
+            costFunction = -np.sum((trainDataLabel*np.log2(self.predictedOutput))) # -Sum(di*log(yi)), where di is the actual output and yi is the predicted output
+        elif (self.costfn == 'squared_error'):
+            costFunction = 0.5*np.sum((self.predictedOutput - trainDataLabel)**2) # 1/2 Sum((yi - di)**2), where di is the actual output and yi is the predicted output
+        # Compute for other cost functions like squared error, etc
+
+        return costFunction
+
+
+    def forwardpass(self, trainDataSample):
+        """ Forward pass"""
+        layerLOutput = trainDataSample
+        self.Ita = []
+        self.outputEachlayer = []
+        # ele3 is looping over the layers
+        for ele3 in range(self.numLayers):
+            numNodesLayerL = self.networkArchitecture[ele3][0]
+            if (ele3 == 0):
+                # Input layer
+                layerLminus1Output = np.ones((numNodesLayerL + 1),dtype=np.float32) # +1 is to account for the bias term
+                layerLminus1Output[1::] = layerLOutput
+            else:
+                weightMatrixLayerLminus1toL = self.weightMatrixList[ele3-1]
+                itaLayerL = weightMatrixLayerLminus1toL @ layerLminus1Output
+                activationFn = self.networkArchitecture[ele3][1] # Activation function name
+                layerLOutput = self.activation_function(itaLayerL,activationFn) # gives output of the activation function for the ita input
+
+                layerLminus1Output = np.ones((numNodesLayerL + 1),dtype=np.float32) # +1 is to account for the bias term
+                layerLminus1Output[1::] = layerLOutput
+
+                self.Ita.append(itaLayerL) # ita is not stored for input layer. It is stored for all other layers.
+            self.outputEachlayer.append(layerLminus1Output) # Output for each layer
+        self.predictedOutput = layerLOutput
+
+
+
+    def backwardpass(self, trainDataLabel):
+        # This backward pass computation is for 'categorical_cross_entropy', 'squared error' loss functions and online mode. Need to add the computation of backward pass for other modes like batch and mini batch
+        # trainDataLabels should also be a 1 hot vector representation
+
+        self.errorEachLayer = []
+        # ele4 loop goes from layer L-1(output) to layer 0 input
+        for ele4 in range(self.numLayers-1,0,-1):
+            if (ele4 == self.numLayers-1):
+                if (self.costfn == 'categorical_cross_entropy'):
+                    errorLayerL = self.predictedOutput - trainDataLabel # (y - d) For softmax activation function with categorical cross entropy cost function. Used for classificcation tasks.
+                elif (self.costfn == 'squared_error'):
+                    itaLayerL = self.Ita[ele4-1]
+                    activationFn = self.networkArchitecture[ele4][1]
+                    activationFnDerivative = self.derivative_activation_function(itaLayerL,activationFn)
+                    errorLayerL = (self.predictedOutput - trainDataLabel) * activationFnDerivative # (y - d)f'(ita_L) For any other activation function(other than softmax) with squared error cost function. Used for regression tasks
+                errorLayerLplus1 = errorLayerL
+            else:
+                weightMatrixLayerLtoLplus1 = self.weightMatrixList[ele4]
+                itaLayerL = self.Ita[ele4-1]
+                activationFn = self.networkArchitecture[ele4][1]
+                activationFnDerivative = self.derivative_activation_function(itaLayerL,activationFn)
+                errorLayerL = (weightMatrixLayerLtoLplus1[:,1::].T @ errorLayerLplus1) * activationFnDerivative # Derivative of the activation function at layer 3 evaluated at input vector at layer 3(nl)
+                errorLayerLplus1 = errorLayerL
+
+            self.errorEachLayer.append(errorLayerLplus1) # These error arrays are packed from layer L-1 down to 1 and not from 1 to L-1. They are arranged in reverse order. (layers start from 0 to L-1)
+        # Errors/delta obtained for all the layers from 2 to L
+
+
+
+    def update_weights(self, stepSize):
+        # Errors/delta obtained for all the layers from 2 to L
+        # Compute gradient wrt Wl_ij
+        # dJ/dWl_ij = deltal+1_j * yi_l
+        # gradient # dJ/dwij for all i,j
+        count = -1
+        for ele4 in range(self.numLayers-1):
+            gradientCostFnwrtWeights = self.errorEachLayer[count][:,None] @ self.outputEachlayer[ele4][None,:]
+            self.weightMatrixList[ele4] = self.weightMatrixList[ele4] - stepSize*gradientCostFnwrtWeights
+            count -= 1
+
 
     def train_nn(self,trainData,trainDataLabels):
         # trainDataLabels should also be a 1 hot vector representation
-        self.backpropagation(trainData,trainDataLabels, mode = 'online',costfn = 'categorical_cross_entropy',epochs=10)
+        self.backpropagation(trainData,trainDataLabels)
 
-    def backpropagation(self,trainData,trainDataLabels, mode = 'online',costfn = 'categorical_cross_entropy',epochs=10):
 
+    def backpropagation(self,trainData,trainDataLabels):
+        """ Currently the back propagation is coded for online mode of weight update. Need to add for batch and mini batch mode"""
         stepSize = 0.01#
         numTrainData = trainData.shape[1]
         # numFeatures = trainData.shape[0]
         arr = np.arange(numTrainData)
-        for ele1 in np.arange(epochs):
+        for ele1 in np.arange(self.epochs):
             np.random.shuffle(arr) # Randomly shuffle the order of feeding the training data for each epoch
             for ele2 in arr:
 
+                trainDataSample = trainData[:,ele2]
+                trainDataLabel = trainDataLabels[:,ele2]
 
+                """ Forward pass"""
+                self.forwardpass(trainDataSample)
 
-                # Forward pass (put into a function)
-                inputVector = np.ones((self.numNodesLayer1+1),dtype=np.float32) # +1 is to account for the bias term
-                inputVector[1::] = trainData[:,ele2] # 1st element of each data vector is 1 and the weight corresponding to 1 is the bias term
-                outputVectorLayer1 = inputVector # y1 = x
+                """ Cost function computation"""
+                costFunctionValue = self.compute_loss_function(trainDataLabel) # Keep appending the cost function value across data points and epochs
 
-                inputVectorLayer2 = self.weightMatrixLayer1to2 @ outputVectorLayer1
-                outputVectorLayer2 = self.ReLU(inputVectorLayer2) # Activation function for layer 2
+                """ Backward pass"""
+                self.backwardpass(trainDataLabel)
 
-                temp = outputVectorLayer2
-                outputVectorLayer2 = np.ones((self.numNodesLayer2+1),dtype=np.float32) # +1 is to account for the bias term
-                outputVectorLayer2[1::] = temp # 1st element of each data vector is 1 and the weight corresponding to 1 is the bias term
-                inputVectorLayer3 = self.weightMatrixLayer2to3 @ outputVectorLayer2
-                outputVectorLayer3 = self.ReLU(inputVectorLayer3) # Activation function for layer 3
-
-                temp = outputVectorLayer3
-                outputVectorLayer3 = np.ones((self.numNodesLayer3+1),dtype=np.float32) # +1 is to account for the bias term
-                outputVectorLayer3[1::] = temp
-                inputVectorLayer4 = self.weightMatrixLayer3to4 @ outputVectorLayer3
-                outputVectorLayer4 = self.softmax(inputVectorLayer4) # Finaly layer output
-
-                costFunction = -np.sum((trainDataLabels[:,ele2]*np.log2(outputVectorLayer4))) # -Sum(di*log(yi)), where di is the actual output and yi is the predicted output
-
-                # Backward pass (put into function)
-                # trainDataLabels should also be a 1 hot vector representation
-                errorLayer4 = outputVectorLayer4 - trainDataLabels[:,ele2] # (y - d) For softmax function with categorical cross entropy cost function
-                errorLayer3 = (self.weightMatrixLayer3to4[:,1::].T @ errorLayer4) * self.ReLU_derivative(inputVectorLayer3) # Derivative of the activation function at layer 3 evaluated at input vector at layer 3(nl)
-                errorLayer2 = (self.weightMatrixLayer2to3[:,1::].T @ errorLayer3) * self.ReLU_derivative(inputVectorLayer2) # Derivative of the activation function at layer 2 evaluated at input vector at layer 2(nl)
-
-                # Errors/delta obtained for all the layers from 2 to L
-                # Compute gradient wrt Wl_ij
-                # dJ/dWl_ij = deltal+1_j * yi_l
-                # gradient # dJ/dwij for all i,j
-                self.weightMatrixLayer1to2 = self.weightMatrixLayer1to2 - stepSize*(errorLayer2[:,None] @ outputVectorLayer1[None,:])
-                self.weightMatrixLayer2to3 = self.weightMatrixLayer2to3 - stepSize*(errorLayer3[:,None] @ outputVectorLayer2[None,:])
-                self.weightMatrixLayer3to4 = self.weightMatrixLayer3to4 - stepSize*(errorLayer4[:,None] @ outputVectorLayer3[None,:])
+                """ Update weights"""
+                self.update_weights(stepSize)
 
 
 
 
 
-mlffnn = MLFFNeuralNetwork()
+
+
+
+""" List of number of nodes, acivation function pairs for each layer.
+1st element in architecture list is input, last element is output"""
+networkArchitecture = [(5,'Identity'), (128,'ReLU'), (128, 'ReLU'), (3,'softmax')]
+mlffnn = MLFFNeuralNetwork(networkArchitecture)
+mlffnn.set_model_params(mode = 'online',costfn = 'categorical_cross_entropy',epochs=10)
 trainData = np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
 trainDataLabels = np.array([[1,0],[0,1],[0,0]])
 mlffnn.train_nn(trainData,trainDataLabels)
