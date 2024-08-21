a34427f5e97620914883e37c10b704e0bbd29f9f
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2024-08-21 23:18:00 +0530

    Implementation of xor gate input ouput mapping using 3 layer NN with sigmoid activation function, 1 hidden layer of 2 nodes and 1 output layer
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

---
+++ deep_neural_networks/xor_gate.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 18:09:19 2024
+
+@author: Sai Gunaranjan
+"""
+
+from multilayer_feedforward_nn import MLFFNeuralNetwork
+import numpy as np
+
+# X_data = np.array([[0,0],[0,1],[1,0],[1,1]]).T
+# Y_data = np.array([[1,0],[0,1],[0,1],[1,0]]).T
+
+
+# """ List of number of nodes, acivation function pairs for each layer.
+# 1st element in architecture list is input, last element is output"""
+# numInputNodes = X_data.shape[0]
+# numOutputNodes = Y_data.shape[0]
+# networkArchitecture = [(numInputNodes,'Identity'), (2, 'sigmoid'), (2,'softmax')]
+# mlffnn = MLFFNeuralNetwork(networkArchitecture)
+# mlffnn.set_model_params(mode = 'online',costfn = 'categorical_cross_entropy',epochs=1000)
+# trainData = X_data
+# trainDataLabels = Y_data
+# mlffnn.train_nn(trainData,trainDataLabels,stepsize=0.1)
+# mlffnn.predict_nn(trainData,trainDataLabels)
+# print('\nActual labels', trainDataLabels[1,:])
+# print('\n Predicted labels', mlffnn.testDataPredictedLabels[1,:])
+
+
+X_data = np.array([[0,0],[0,1],[1,0],[1,1]]).T
+Y_data = np.array([0,1,1,0])[None,:]
+
+
+""" List of number of nodes, acivation function pairs for each layer.
+1st element in architecture list is input, last element is output"""
+numInputNodes = X_data.shape[0]
+numOutputNodes = Y_data.shape[0]
+networkArchitecture = [(numInputNodes,'Identity'), (2, 'sigmoid'), (1,'sigmoid')]
+mlffnn = MLFFNeuralNetwork(networkArchitecture)
+mlffnn.set_model_params(mode = 'online',costfn = 'squared_error',epochs=10000)
+trainData = X_data
+trainDataLabels = Y_data
+mlffnn.train_nn(trainData,trainDataLabels,stepsize=0.1)
+mlffnn.predict_nn(trainData,trainDataLabels)
+mlffnn.testDataPredictedLabels[mlffnn.testDataPredictedLabels>=0.5] = 1
+mlffnn.testDataPredictedLabels[mlffnn.testDataPredictedLabels<0.5] = 0
+print('\nActual labels', trainDataLabels[0,:])
+print('\n Predicted labels', mlffnn.testDataPredictedLabels[0,:])
