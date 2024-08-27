8aeebafbbbf750d83509c22bf37c555479e0d2e1
Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>
2024-08-27 22:14:38 +0530

    Use NN to predict output of XOR gate both as a regression as well as classification problem
    Signed-off-by: Sai Gunaranjan Pelluri <saigunaranjanpelluri@gmail.com>

--- deep_neural_networks/xor_gate.py
+++ deep_neural_networks/xor_gate.py
@@ -9,23 +9,33 @@ from multilayer_feedforward_nn import MLFFNeuralNetwork
 import numpy as np
 import matplotlib.pyplot as plt
 
-# """ NN for classification"""
-# X_data = np.array([[0,0],[0,1],[1,0],[1,1]]).T
-# Y_data = np.array([[1,0],[0,1],[0,1],[1,0]]).T
+""" NN for classification"""
+X_data = np.array([[0,0],[0,1],[1,0],[1,1]]).T
+Y_data = np.array([[1,0],[0,1],[0,1],[1,0]]).T # One hot vector
 
-# """ List of number of nodes, acivation function pairs for each layer.
-# 1st element in architecture list is input, last element is output"""
-# numInputNodes = X_data.shape[0]
-# numOutputNodes = Y_data.shape[0]
-# networkArchitecture = [(numInputNodes,'Identity'), (2, 'sigmoid'), (2,'softmax')]
-# mlffnn = MLFFNeuralNetwork(networkArchitecture)
-# mlffnn.set_model_params(mode = 'online',costfn = 'categorical_cross_entropy',epochs=100000, stepsize=0.1)
-# trainData = X_data
-# trainDataLabels = Y_data
-# mlffnn.train_nn(trainData,trainDataLabels)
-# mlffnn.predict_nn(trainData,trainDataLabels)
-# print('\nActual labels', trainDataLabels[1,:])
-# print('\n Predicted labels', mlffnn.testDataPredictedLabels[1,:])
+""" List of number of nodes, acivation function pairs for each layer.
+1st element in architecture list is input, last element is output"""
+numInputNodes = X_data.shape[0]
+numOutputNodes = Y_data.shape[0]
+networkArchitecture = [(numInputNodes,'Identity'), (2, 'sigmoid'), (2,'softmax')]
+mlffnn = MLFFNeuralNetwork(networkArchitecture)
+mlffnn.set_model_params(mode = 'online',costfn = 'categorical_cross_entropy',epochs=10000, stepsize=0.1)
+trainData = X_data
+trainDataLabels = Y_data
+mlffnn.train_nn(trainData,trainDataLabels)
+mlffnn.predict_nn(trainData,trainDataLabels)
+mlffnn.testDataPredictedLabels[mlffnn.testDataPredictedLabels>=0.5] = 1
+mlffnn.testDataPredictedLabels[mlffnn.testDataPredictedLabels<0.5] = 0
+print('\nActual labels', trainDataLabels[1,:])
+print('\n Predicted labels', mlffnn.testDataPredictedLabels[1,:])
+
+numTrainingSamples = trainData.shape[1]
+
+plt.figure(1,figsize=(20,10),dpi=200)
+plt.title('Cost / loss function')
+plt.plot(mlffnn.costFunctionVal[0::numTrainingSamples])
+plt.xlabel('Epochs')
+plt.grid(True)
 
 """ NN for regression"""
 X_data = np.array([[0,0],[0,1],[1,0],[1,1]]).T
@@ -49,8 +59,7 @@ print('\n Predicted labels', mlffnn.testDataPredictedLabels[0,:])
 
 numTrainingSamples = trainData.shape[1]
 
-
-plt.figure(1,figsize=(20,10),dpi=200)
+plt.figure(2,figsize=(20,10),dpi=200)
 plt.title('Cost / loss function')
 plt.plot(mlffnn.costFunctionVal[0::numTrainingSamples])
 plt.xlabel('Epochs')
